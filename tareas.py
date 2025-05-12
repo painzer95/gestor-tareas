5941b81815a3d22f44cea2b6cb532afac1a8f78b
painzer95 <jesusitolopez10@gmail.com>
2025-05-12 18:39:46 +0200

    Añadida función listar_tareas

--- tareas.py
+++ tareas.py
@@ -1,3 +1,12 @@
 def agregar_tarea(lista, tarea):
     lista.append(tarea)
     return lista
+
+
+def listar_tareas(lista):
+    if not lista:
+        print("No hay tareas en la lista.")
+    else:
+        print("Lista de tareas:")
+        for i, t in enumerate(lista):
+            print(f"{i + 1}. {t}")
